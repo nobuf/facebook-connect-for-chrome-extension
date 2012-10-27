3f3b6234025c493a8971dcffd1855a8d6b4635a8
Vinit <vinitcool76@gmail.com>
2012-10-27 12:33:26 +0530

    now gets only access token instead of expiry date

--- background.js
+++ background.js
@@ -5,8 +5,9 @@ function onFacebookLogin() {
                         for (var i = 0; i < tabs.length; i++) {
                             if (tabs[i].url.indexOf(successURL) == 0) {
                                 var params = tabs[i].url.split('#')[1];
-                                console.log(params);
-                                localStorage.accessToken = params;
+				access = params.split('&')[0]
+                                console.log(access);
+                                localStorage.accessToken = access;
                                 chrome.tabs.onUpdated.removeListener(onFacebookLogin);
                                 return;
                             }
